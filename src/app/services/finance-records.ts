8b2cdf635c226be12e98e750c2f18e2010489458
Eduardo Carrazzone <eduardocc82@gmail.com>
2026-05-20 23:19:59 -0300

    mudanda

--- src/app/services/finance-records.ts
+++ src/app/services/finance-records.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject } from 'rxjs';
 
 import { FinancialRecord, NewFinancialRecord, TransactionType } from '../models/finance.models';
 
+
 type MockRecordRow = readonly [
   id: string,
   description: string,
